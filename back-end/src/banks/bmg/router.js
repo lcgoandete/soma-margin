c9e7e9ad0d132a8350a2826ad76f1d69319340e4
lcgoandete <lcgoandete@hotmail.com>
2023-07-01 09:49:16 -0300

    back-feat: add bmg register proposal card

--- back-end/src/banks/bmg/router.js
+++ back-end/src/banks/bmg/router.js
@@ -5,6 +5,7 @@ const { validateToken } = require('../../middlewares/token');
 const { validateCpf } = require('../../middlewares/validate-cpf');
 const withdrawalLimit = require('./controllers/withdrawal-limit');
 const complementaryWithdrawal = require('./controllers/complementaryWithdrawal');
+const registerProposal = require('./controllers/register-proposal-card');
 
 complementaryWithdrawalRoutes.post(
   '/banks/bmg/complementary-withdrawal/card-limit',
@@ -17,6 +18,12 @@ complementaryWithdrawalRoutes.post(
   '/banks/bmg/withdrawal-limit',
   validateToken,
   rescue(withdrawalLimit.getWithdrawalLimit),
+);
+
+complementaryWithdrawalRoutes.post(
+  '/banks/bmg/register-proposal-card',
+  validateToken,
+  rescue(registerProposal.registerProposalCard),
 );
 
 module.exports = complementaryWithdrawalRoutes;
